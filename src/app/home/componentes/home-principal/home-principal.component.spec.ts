dc99b4a120aae7895707dabe02a3bf45ebcfb0aa
Rafael <rafaelsoec@gmail.com>
2021-03-08 02:47:31 -0300

    asda

---
+++ src/app/home/componentes/home-principal/home-principal.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HomePrincipalComponent } from './home-principal.component';
+
+describe('HomePrincipalComponent', () => {
+  let component: HomePrincipalComponent;
+  let fixture: ComponentFixture<HomePrincipalComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HomePrincipalComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomePrincipalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
