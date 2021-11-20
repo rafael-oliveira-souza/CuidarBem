a820cf63edc2ec39a1d38a14d7d15aee121be0c0
Rafael Souza <rafaelsoec@gmail.com>
2021-11-19 23:42:56 -0300

    Ajustes

--- src/app/home/componentes/home-principal/home-principal.component.ts
+++ src/app/home/componentes/home-principal/home-principal.component.ts
@@ -6,9 +6,22 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./home-principal.component.scss"],
 })
 export class HomePrincipalComponent implements OnInit {
+  public heightHeader: number = 0;
+
   constructor() {}
 
   ngOnInit() {
+    let header = document.querySelector(".header");
+    let headerAcoes = document.querySelector(".header-acoes");
+    let headerMenu = document.querySelector(".header-menu");
+    let headerBorda = document.querySelector(".header-borda");
+
+    // this.heightHeader += header.clientHeight + header.scrollHeight;
+    // this.heightHeader += headerAcoes.clientHeight + headerAcoes.scrollHeight;
+    // this.heightHeader += headerMenu.clientHeight + headerMenu.scrollHeight;
+    // this.heightHeader += headerBorda.clientHeight + headerBorda.scrollHeight;
+
+    // this.heightHeader = header + headerMenu + headerBorda + "px";
     // const objetoEnvio: ObjetoEnvio = this._produtoService.instanciarObjetoEnvio();
   }
 }
